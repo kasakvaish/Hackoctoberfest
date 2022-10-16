cb4fcfedd62b809edb623a17cf41f336e5b50288
SKR301 <skrint301@gmail.com>
2022-10-16 19:48:09 +0530

    fixed typo and error #24

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 int main() {
-   int sea, c, n=6;
-   int arr[] = { 12, 35, 69, 74, 165, 54};
-   sea=165;
+   int search, c, n = 6;
+   int arr[] = { 12, 35, 69, 74, 165, 54 };
+   search=165;
+
    for (c = 0; c < n; c++) {
-      if (arr[c] == sea) {
+      if (arr[c] == search) {
          printf("%d is present at location %d.\n", search, c+1);
          break;
       }
    }
+   
    if (c == n)
       printf("%d isn't present in the array.\n", search);
+   
    return 0;
 }
